5588a5c2d1f71f1cf96f260c7e21f57108c9d89f
weiqiyiji <weiqiyiji@gmail.com>
2018-09-06 15:44:56 +0800

    enable go mode
    Change-Id: Iabedd82a7541cc4fdc873985e918fc41dca507cb

--- init.el
+++ init.el
@@ -48,7 +48,7 @@ values."
      ;;        shell-default-height 30
      ;;        shell-default-position 'bottom)
      ;; spell-checking
-     ;; syntax-checking
+     syntax-checking
      ;; version-control
      )
    ;; List of additional packages that will be installed without being
@@ -319,6 +319,8 @@ you should place your code here."
     (setq mac-command-modifier 'meta)
     (global-set-key [kp-delete] 'delete-char) ;; sets fn-delete to be right-delete
     )
+  (when (version<= "26.0.50" emacs-version )
+    (global-display-line-numbers-mode))
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
@@ -330,7 +332,125 @@ you should place your code here."
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (go-guru go-eldoc go-mode ws-butler winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline powerline restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox spinner org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint indent-guide hydra hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation helm-themes helm-swoop helm-projectile helm-mode-manager helm-make projectile pkg-info epl helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu highlight elisp-slime-nav dumb-jump f dash s diminish define-word column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core popup async))))
+    (flycheck-pos-tip
+     pos-tip
+     flycheck
+     helm-company
+     helm-c-yasnippet
+     fuzzy
+     company
+     auto-yasnippet
+     yasnippet
+     ac-ispell
+     auto-complete
+     go-guru
+     go-eldoc
+     go-mode
+     ws-butler
+     winum
+     which-key
+     volatile-highlights
+     vi-tilde-fringe
+     uuidgen
+     use-package
+     toc-org
+     spaceline
+     powerline
+     restart-emacs
+     request
+     rainbow-delimiters
+     popwin
+     persp-mode
+     pcre2el
+     paradox
+     spinner
+     org-plus-contrib
+     org-bullets
+     open-junk-file
+     neotree
+     move-text
+     macrostep
+     lorem-ipsum
+     linum-relative
+     link-hint
+     indent-guide
+     hydra
+     hungry-delete
+     hl-todo
+     highlight-parentheses
+     highlight-numbers
+     parent-mode
+     highlight-indentation
+     helm-themes
+     helm-swoop
+     helm-projectile
+     helm-mode-manager
+     helm-make
+     projectile
+     pkg-info
+     epl
+     helm-flx
+     helm-descbinds
+     helm-ag
+     google-translate
+     golden-ratio
+     flx-ido
+     flx
+     fill-column-indicator
+     fancy-battery
+     eyebrowse
+     expand-region
+     exec-path-from-shell
+     evil-visualstar
+     evil-visual-mark-mode
+     evil-unimpaired
+     evil-tutor
+     evil-surround
+     evil-search-highlight-persist
+     evil-numbers
+     evil-nerd-commenter
+     evil-mc
+     evil-matchit
+     evil-lisp-state
+     smartparens
+     evil-indent-plus
+     evil-iedit-state
+     iedit
+     evil-exchange
+     evil-escape
+     evil-ediff
+     evil-args
+     evil-anzu
+     anzu
+     evil
+     goto-chg
+     undo-tree
+     eval-sexp-fu
+     highlight
+     elisp-slime-nav
+     dumb-jump
+     f
+     dash
+     s
+     diminish
+     define-word
+     column-enforce-mode
+     clean-aindent-mode
+     bind-map
+     bind-key
+     auto-highlight-symbol
+     auto-compile
+     packed
+     aggressive-indent
+     adaptive-wrap
+     ace-window
+     ace-link
+     ace-jump-helm-line
+     helm
+     avy
+     helm-core
+     popup
+     async))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
