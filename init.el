b18d70aa7fe2503107f708fb9c9a5823c93c97d5
weiqiyiji <weiqiyiji@gmail.com>
2018-09-06 20:56:11 +0800

    Add graphql mode
    Change-Id: I0477872f5474df71e68ee9cdf63b25d3f2677592

--- init.el
+++ init.el
@@ -31,6 +31,7 @@ values."
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
    '(
+     yaml
      go
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
@@ -55,7 +56,7 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '()
+   dotspacemacs-additional-packages '(graphql-mode)
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
@@ -228,7 +229,7 @@ values."
    ;; If non nil the frame is maximized when Emacs starts up.
    ;; Takes effect only if `dotspacemacs-fullscreen-at-startup' is nil.
    ;; (default nil) (Emacs 24.4+ only)
-   dotspacemacs-maximized-at-startup nil
+   dotspacemacs-maximized-at-startup t
    ;; A value from the range (0..100), in increasing opacity, which describes
    ;; the transparency level of a frame when it's active or selected.
    ;; Transparency can be toggled through `toggle-transparency'. (default 90)
@@ -305,6 +306,7 @@ before packages are loaded. If you are unsure, you should try in setting them in
         '(("melpa-cn" . "http://elpa.emacs-china.org/melpa/")
           ("org-cn"   . "http://elpa.emacs-china.org/org/")
           ("gnu-cn"   . "http://elpa.emacs-china.org/gnu/")))
+  (setq line-spacing 0.3)
   (setq-default dotspacemacs-default-font '("Source Code Pro"
                                             :size 15
                                             :weight normal
@@ -327,6 +329,8 @@ you should place your code here."
     (setq powerline-default-separator 'utf-8)
     (global-set-key [kp-delete] 'delete-char) ;; sets fn-delete to be right-delete
     )
+
+  (setq org-startup-folded nil)
   )
 
 
@@ -343,125 +347,7 @@ you should place your code here."
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (flycheck-pos-tip
-     pos-tip
-     flycheck
-     helm-company
-     helm-c-yasnippet
-     fuzzy
-     company
-     auto-yasnippet
-     yasnippet
-     ac-ispell
-     auto-complete
-     go-guru
-     go-eldoc
-     go-mode
-     ws-butler
-     winum
-     which-key
-     volatile-highlights
-     vi-tilde-fringe
-     uuidgen
-     use-package
-     toc-org
-     spaceline
-     powerline
-     restart-emacs
-     request
-     rainbow-delimiters
-     popwin
-     persp-mode
-     pcre2el
-     paradox
-     spinner
-     org-plus-contrib
-     org-bullets
-     open-junk-file
-     neotree
-     move-text
-     macrostep
-     lorem-ipsum
-     linum-relative
-     link-hint
-     indent-guide
-     hydra
-     hungry-delete
-     hl-todo
-     highlight-parentheses
-     highlight-numbers
-     parent-mode
-     highlight-indentation
-     helm-themes
-     helm-swoop
-     helm-projectile
-     helm-mode-manager
-     helm-make
-     projectile
-     pkg-info
-     epl
-     helm-flx
-     helm-descbinds
-     helm-ag
-     google-translate
-     golden-ratio
-     flx-ido
-     flx
-     fill-column-indicator
-     fancy-battery
-     eyebrowse
-     expand-region
-     exec-path-from-shell
-     evil-visualstar
-     evil-visual-mark-mode
-     evil-unimpaired
-     evil-tutor
-     evil-surround
-     evil-search-highlight-persist
-     evil-numbers
-     evil-nerd-commenter
-     evil-mc
-     evil-matchit
-     evil-lisp-state
-     smartparens
-     evil-indent-plus
-     evil-iedit-state
-     iedit
-     evil-exchange
-     evil-escape
-     evil-ediff
-     evil-args
-     evil-anzu
-     anzu
-     evil
-     goto-chg
-     undo-tree
-     eval-sexp-fu
-     highlight
-     elisp-slime-nav
-     dumb-jump
-     f
-     dash
-     s
-     diminish
-     define-word
-     column-enforce-mode
-     clean-aindent-mode
-     bind-map
-     bind-key
-     auto-highlight-symbol
-     auto-compile
-     packed
-     aggressive-indent
-     adaptive-wrap
-     ace-window
-     ace-link
-     ace-jump-helm-line
-     helm
-     avy
-     helm-core
-     popup
-     async))))
+    (graphql-mode yaml-mode flycheck-pos-tip pos-tip flycheck helm-company helm-c-yasnippet fuzzy company auto-yasnippet yasnippet ac-ispell auto-complete go-guru go-eldoc go-mode ws-butler winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline powerline restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox spinner org-plus-contrib org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint indent-guide hydra hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation helm-themes helm-swoop helm-projectile helm-mode-manager helm-make projectile pkg-info epl helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg undo-tree eval-sexp-fu highlight elisp-slime-nav dumb-jump f dash s diminish define-word column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed aggressive-indent adaptive-wrap ace-window ace-link ace-jump-helm-line helm avy helm-core popup async))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
